a5b04acb60f82992f15d04089d9cf35ea4eb7854
chris.ng93 <chris.ng93@gmail.com>
2016-05-25 20:53:39 -0700

    create basic structure for files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 var session = require('express-session');
 var express = require('express');
 var app = express();
-
+var db = require('./db/config');
+var route = require('/routes');
 
 app.use(express.static(__dirname + '/public'));
 
