b3cb3f8312e1bca379203ac10f3f38c55e7db9e4
Katherine <katherine.sj@gmail.com>
2016-06-07 17:19:43 -0700

    moved auth-routes

--- server/app.js
+++ server/app.js
@@ -251,18 +251,20 @@ app.post('/sellItem', (req, res) => {
 });
 
 // ########################### FACEBOOK OAUTH ###########################
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
+require('./routes/auth-routes.js')(app, passport);
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/' }));
+// app.get('/auth/facebook',
+//   passport.authenticate('facebook'));
 
-app.get('/signout' , (req, res) => {
-  // check to see if this actually works
-  req.session.destroy();
-  res.redirect('/');
-});
+// app.get('/auth/facebook/callback',
+//   passport.authenticate('facebook', { successRedirect: '/',
+//                                       failureRedirect: '/' }));
+
+// app.get('/signout' , (req, res) => {
+//   // check to see if this actually works
+//   req.session.destroy();
+//   res.redirect('/');
+// });
 
 // require('./initialize/auth-init.js')(passport, FacebookStrategy);
 
