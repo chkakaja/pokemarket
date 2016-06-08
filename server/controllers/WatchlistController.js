5a154e50706adde0c32fed4348a1882581312acd
Katherine <katherine.sj@gmail.com>
2016-06-08 16:33:34 -0700

    modularized ItemControllers

--- server/controllers/WatchlistController.js
+++ server/controllers/WatchlistController.js
@@ -1,1 +1,22 @@
 var WatchList = require('./../db/models/watchlist.js');
+var Message = require('./../db/models/message.js');
+var User = require('./../db/models/user.js');
+var db = require('./../db/config.js');
+
+module.exports = {
+  watchItem: (req, res) => {
+    WatchList.where({ user_id: req.query.user_id, item_id: req.query.item_id }).fetch()
+      .then(function(result) {
+        if (result === null) {
+          new WatchList(req.query).save().then(() => res.send(req.query.item_id));
+        } else {
+          result.destroy();
+          res.send('Unwatched item');
+        }
+      })
+      .catch(function(err) {
+        res.send('Error:', err);
+      });
+  }
+
+}
