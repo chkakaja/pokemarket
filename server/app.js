0db4726930229b5a5458e84470e6e5d610eec6de
Katherine <katherine.sj@gmail.com>
2016-06-07 17:25:38 -0700

    moved feedback-routes

--- server/app.js
+++ server/app.js
@@ -62,41 +62,42 @@ app.get('/getprofile', (req, res) => {
 
 
 // ############################## FEEDBACK ##############################
+require('./routes/feedback-routes.js')(app);
 
-app.get('/feedback', (req, res) => {
-  db.knex('feedback')
-    .where({ receiver_id: req.query.receiver })
-    .innerJoin('users', 'feedback.author_id', '=', 'users.id')
-    .then(feedbackArray => {
-      res.status(200).send(feedbackArray);
-    });
-});
+// app.get('/feedback', (req, res) => {
+//   db.knex('feedback')
+//     .where({ receiver_id: req.query.receiver })
+//     .innerJoin('users', 'feedback.author_id', '=', 'users.id')
+//     .then(feedbackArray => {
+//       res.status(200).send(feedbackArray);
+//     });
+// });
 
-app.get('/toleavefeedback', (req, res) => {
-  if (req.session.hasOwnProperty('passport')) {
-    User
-      .where({ facebookId: req.session.passport.user })
-      .fetch()
-      .then(user => {
-        db.knex('items')
-          .where({ current_bidder: user.id, feedback: null })
-          .andWhere('end_at', '<', new Date())
-          // 2016-06-06T23:11:07.828Z
-          // 2016-06-12 15:59:22.632
-          .then(items => res.status(200).send(items));
-      });
-  }
-  res.status(404);
-});
+// app.get('/toleavefeedback', (req, res) => {
+//   if (req.session.hasOwnProperty('passport')) {
+//     User
+//       .where({ facebookId: req.session.passport.user })
+//       .fetch()
+//       .then(user => {
+//         db.knex('items')
+//           .where({ current_bidder: user.id, feedback: null })
+//           .andWhere('end_at', '<', new Date())
+//           // 2016-06-06T23:11:07.828Z
+//           // 2016-06-12 15:59:22.632
+//           .then(items => res.status(200).send(items));
+//       });
+//   }
+//   res.status(404);
+// });
 
-app.post('/leavefeedback', (req, res) => {
-  new Feedback(req.body)
-    .save()
-    .then(feedback => {
-      new Item({ id: req.body.item_id, feedback: feedback.attributes.id });
-      res.send();
-    });
-});
+// app.post('/leavefeedback', (req, res) => {
+//   new Feedback(req.body)
+//     .save()
+//     .then(feedback => {
+//       new Item({ id: req.body.item_id, feedback: feedback.attributes.id });
+//       res.send();
+//     });
+// });
 // ############################## MESSAGES #############################
 
 app.post('/getMessages', (req, res) => {
