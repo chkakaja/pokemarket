606e506bc74b194bc09d2a4f8eda6f5ebcceef34
Katherine <katherine.sj@gmail.com>
2016-06-08 16:12:36 -0700

    modularized usercontrollers

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,3 +1,23 @@
 var User = require('./../db/models/user.js');
 
+module.exports = {
+  getUserId: (req, res) => {
+    if (req.session.hasOwnProperty('passport')) {
+      User
+        .where({ facebookId: req.session.passport.user })
+        .fetch()
+        .then(user => {
+          res.status(200).send(user);
+        });
+      return;
+    }
+    res.sendStatus(404);
+  },
 
+  getProfile: (req, res) => {
+   User
+     .where({ id: req.query.id })
+     .fetch()
+     .then(user => res.send(user));
+   }
+}
--- server/routes/profile-routes.js
+++ server/routes/profile-routes.js
@@ -1,12 +1,15 @@
-var User = require('./../db/models/user');
+// var User = require('./../db/models/user');
+var UserController = require('./../controllers/UserController.js')
 
 
 
 module.exports = function(app) {
- app.get('/getprofile', (req, res) => {
-   User
-     .where({ id: req.query.id })
-     .fetch()
-     .then(user => res.send(user));
- });
+ app.get('/getprofile', UserController.getProfile);
+
+ //  (req, res) => {
+ //   User
+ //     .where({ id: req.query.id })
+ //     .fetch()
+ //     .then(user => res.send(user));
+ // });
 }
