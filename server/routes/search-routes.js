b61caa0590967e6acce45ecb978068bd43462c69
Katherine <katherine.sj@gmail.com>
2016-06-08 16:39:35 -0700

    modularize searchcontroller

--- server/routes/search-routes.js
+++ server/routes/search-routes.js
@@ -1,19 +1,21 @@
-var db = require('./../db/config');
+// var db = require('./../db/config');
+var SearchController = require('./../controllers/SearchController.js')
 
 
 module.exports = function(app) {
-  app.post('/search', (req, res) => {
-    db.knex('items')
-      .where('title', 'like', '%' + req.body.search + ' %')
-      .orWhere('title', 'like', '% ' + req.body.search + '%')
-      .orWhere('title', 'like', '% ' + req.body.search + ' %')
-      .orWhere('title', '=', req.body.search)
-      .then(items => {
-        console.log(items);
-        res.send(items);
-      })
-      .catch(err => {
-        res.send('Error:', err)
-      });
-  });
+  app.post('/search', SearchController.search);
+  //   (req, res) => {
+  //   db.knex('items')
+  //     .where('title', 'like', '%' + req.body.search + ' %')
+  //     .orWhere('title', 'like', '% ' + req.body.search + '%')
+  //     .orWhere('title', 'like', '% ' + req.body.search + ' %')
+  //     .orWhere('title', '=', req.body.search)
+  //     .then(items => {
+  //       console.log(items);
+  //       res.send(items);
+  //     })
+  //     .catch(err => {
+  //       res.send('Error:', err)
+  //     });
+  // });
 }
