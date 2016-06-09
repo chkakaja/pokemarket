5f89dd5d4e33eb2ee17ee3b812f699fcb2cec85c
Christian Haug <csh@christianhaug.com>
2016-06-09 15:28:41 -0700

    remove controller code from routes

--- server/routes/profile-routes.js
+++ server/routes/profile-routes.js
@@ -1,7 +1,4 @@
-// var User = require('./../db/models/user');
 var UserController = require('./../controllers/UserController.js')
-
-
 
 module.exports = function(app) {
  app.get('/getprofile', UserController.getProfile);
--- server/routes/search-routes.js
+++ server/routes/search-routes.js
@@ -1,7 +1,6 @@
 // var db = require('./../db/config');
 var SearchController = require('./../controllers/SearchController.js')
 
-
 module.exports = function(app) {
   app.post('/search', SearchController.search);
 }
