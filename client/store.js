f6a6a1d663322824a2eaf916997c8151c000a9ed
Jack Zhang <15jzhang@gmail.com>
2016-06-08 19:28:23 -0700

    Move root reducer composition into separate rootReducer file

--- client/store.js
+++ client/store.js
@@ -1,36 +1,9 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-
-import user from './reducers/user.js';
-import profile from './reducers/profile.js';
-import messages from './reducers/messages.js';
-import { reducer as form } from 'redux-form';
-import currentItem from './reducers/currentItem.js';
-import filteredItems from './reducers/filteredItems.js';
-import watchedItems from './reducers/watchedItems.js';
-import listedItems from './reducers/listedItems.js';
-import popularItems from './reducers/popularItems.js';
 import initialState from './initialState.js'
-import feedback from './reducers/feedback.js';
-import toLeaveFeedback from './reducers/toLeaveFeedback.js';
-
-var reducers = combineReducers({
-  user,
-  profile,
-  messages,
-  form,
-  currentItem,
-  watchedItems,
-  filteredItems,
-  listedItems,
-  popularItems,
-  feedback,
-  toLeaveFeedback
-});
 
 export default createStore(
-  reducers, 
+  rootReducer,
   initialState(), 
   applyMiddleware(thunkMiddleware)
 );
-
