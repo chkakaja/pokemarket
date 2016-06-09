35e918e85b97a9edd5ce2c79df7c5aaf7a65b4e9
Christian Haug <csh@christianhaug.com>
2016-06-09 14:20:24 -0700

    remove controller code from routes

--- server/routes/message-routes.js
+++ server/routes/message-routes.js
@@ -5,34 +5,6 @@ var UserController = require('./../controllers/UserController.js')
 
 module.exports = function(app) {
   app.post('/getMessages', MessageController.getMessages);
-
-  //  (req, res) => {
-  //   Message
-  //   .query({ where: { sender: req.body.sender, receiver: req.body.receiver },
-  //            orWhere: { sender: req.body.receiver, receiver: req.body.sender }})
-  //   .orderBy('created_at', 'ASC')
-  //   .fetchAll()
-  //   .then(messages => {
-  //     res.status(200).send(messages);
-  //   });
-  // });
-
   app.post('/sendMessage', MessageController.sendMessage);
-  //   (req, res) => {
-  //   new Message(req.body).save().then(() => res.status(200));
-  // });
-
-  app.get('/getuserid', UserController.getUserId); 
-  //   (req, res) => {
-  //   if (req.session.hasOwnProperty('passport')) {
-  //     User
-  //       .where({ facebookId: req.session.passport.user })
-  //       .fetch()
-  //       .then(user => {
-  //         res.status(200).send(user);
-  //       });
-  //     return;
-  //   }
-  //   res.sendStatus(404);
-  // });
+  app.get('/getuserid', UserController.getUserId);
 }
