088f99acbbba496ce570eb21f3520839a9a1699d
Jack Zhang <15jzhang@gmail.com>
2016-06-11 14:10:45 -0700

    Adjust webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 var marketConfig = {
   context: __dirname + '/client/market',
-  entry: './index.jsx',
+  entry: './index.js',
 
   output: {
     filename: 'market.bundle.js',
@@ -24,7 +24,7 @@ var marketConfig = {
 
 var haggleConfig = {
   context: __dirname + '/client/haggle',
-  entry: './index.jsx',
+  entry: './index.js',
 
   output: {
     filename: 'haggle.bundle.js',
