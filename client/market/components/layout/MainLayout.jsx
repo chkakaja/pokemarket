ea0ec888409b33406621bc62d4dac4465ce980a6
Jack Zhang <15jzhang@gmail.com>
2016-06-11 04:00:35 -0700

    Generate random video chat session ids

--- client/market/components/layout/MainLayout.jsx
+++ client/market/components/layout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React, { Proptypes } from 'react';
+
 import Navbar from './NavBar.jsx';
 import MessageBoxes from './../message-boxes/MessageBoxes.jsx';
 
@@ -11,11 +12,12 @@ export default class App extends React.Component {
   }
 
   render() {
+    let haggleUrl = '/haggle?' + 'id=' + Math.floor((Math.random() * 10000000)).toString() + '&';
     return (
       <div className='main-layout'>
         <Navbar />
-          <a href={'/haggle?' + 'id=2345u435982345' + '&' + 'init=1'} target="_blank">Video Initiate</a>
-          <a href={'/haggle?' + 'id=2345u435982345' + '&' + 'init=0' } target="_blank">Video Respond</a>
+          <a href={haggleUrl +'init=1'} target="_blank">Video Initiate</a>
+          <a href={haggleUrl +'init=0'} target="_blank">Video Respond</a>
           {this.props.children}
         <MessageBoxes />
       </div>
