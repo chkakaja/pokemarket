348f5a805005e2fee70994dffbf4f8647e0674b0
Jack Zhang <15jzhang@gmail.com>
2016-06-10 23:01:14 -0700

    Add webpack configuration for haggle

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ var marketConfig = {
   entry: './index.js',
 
   output: {
-    filename: "market.bundle.js",
-    path: __dirname + "/public",
+    filename: 'market.bundle.js',
+    path: __dirname + '/public',
   },
   module: {
     loaders: [
@@ -22,4 +22,26 @@ var marketConfig = {
   }
 };
 
-module.exports = marketConfig;
+var haggleConfig = {
+  context: __dirname + '/client/haggle',
+  entry: './index.js',
+
+  output: {
+    filename: 'haggle.bundle.js',
+    path: __dirname + '/public',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$|.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ],
+  }
+};
+
+module.exports = [ marketConfig, haggleConfig ];
